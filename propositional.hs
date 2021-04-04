86f5a52027a53319f8a7daab25d88435966ee20d
Aidan Bailey <BLYAID001@myuct.ac.za>
2021-04-04 09:43:50 +0200

    implemented native parser

--- propositional.hs
+++ propositional.hs
@@ -1,25 +1,158 @@
--- | Propositional module
-module Propositional where
+-- | Propositional Logic module
+module PL where
 
--- | variable value map
-type Assoc k v = [(k, v)]
+-------------
+-- IMPORTS --
+-------------
+import Control.Monad
+import System.IO
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Expr
+import Text.ParserCombinators.Parsec.Language
+import qualified Text.ParserCombinators.Parsec.Token as Token
 
--- | formula of logic
+---------------
+-- DATATYPES --
+---------------
+
+-- | type for formula of logic
 data Form
   = Const Bool
-  | Atom Char
+  | Atom String
   | Not Form
   | And Form Form
   | Or Form Form
   | Imply Form Form
   | Iff Form Form
-  deriving (Show)
 
--- | subsitution type to store atom values
-type Interpretation = Assoc Char Bool
+-- | variable value map
+type Substs k v = [(k, v)]
 
--- | find function takes in
-find :: Char -> Interpretation -> Bool
+-- | substitution type to store atom values
+type Interpretation = Substs String Bool
+
+--------------
+-- INSTANCE --
+--------------
+
+-- | Show instance of form
+instance Show Form where
+  show (Atom c) = show c
+  show (Not f) = "¬" ++ show f
+  show (And f1 f2) = show f1 ++ "∧" ++ show f2
+  show (Or f1 f2) = show f1 ++ "∨" ++ show f2
+  show (Imply f1 f2) = show f1 ++ "→" ++ show f2
+  show (Iff f1 f2) = show f1 ++ "↔" ++ show f2
+
+------------------
+-- LEXER/PARSER --
+------------------
+
+-- | language definition
+languageDef =
+  emptyDef
+    { Token.commentStart = "/*",
+      Token.commentEnd = "*/",
+      Token.commentLine = "//",
+      Token.identStart = letter,
+      Token.identLetter = alphaNum,
+      Token.caseSensitive = True,
+      Token.reservedNames =
+        [ "Const",
+          "Not",
+          "And",
+          "Or",
+          "Imply",
+          "Iff",
+          "True",
+          "False"
+        ],
+      Token.reservedOpNames =
+        [ "Not",
+          "And",
+          "Or",
+          "Imply",
+          "Iff"
+        ]
+    }
+
+-- lexer
+lexer = Token.makeTokenParser languageDef
+
+-- parses an identifier
+identifier = Token.identifier lexer
+
+-- parses a reserved name
+reserved = Token.reserved lexer
+
+-- parses an operator
+reservedOp = Token.reservedOp lexer
+
+-- parses surrounding parenthesis
+parens = Token.parens lexer
+
+--   parens p
+-- takes care of the parenthesis and
+-- uses p to parse what's inside them
+
+-- parses whitespace
+whiteSpace = Token.whiteSpace lexer
+
+-- propositional parser start
+propositionalParser :: Parser Form
+propositionalParser = whiteSpace >> formula
+
+-- parser formula
+formula :: Parser Form
+formula = buildExpressionParser operators term
+
+-- operators
+operators =
+  [ [Prefix (reservedOp "Not" >> return Not)],
+    [ Infix (reservedOp "And" >> return And) AssocLeft,
+      Infix (reservedOp "Or" >> return Or) AssocLeft,
+      Infix (reservedOp "Imply" >> return Imply) AssocLeft,
+      Infix (reservedOp "Iff" >> return Iff) AssocNone
+    ]
+  ]
+
+-- terms nested in formulas
+term =
+  parens formula
+    <|> atom
+    <|> constBool
+
+-- an single boolean variable
+atom =
+  do Atom <$> identifier
+
+-- const boolean statement
+constBool =
+  (reserved "True" >> return (Const True))
+    <|> (reserved "False" >> return (Const False))
+
+-- | parseString function parses a string into a logical formula
+parseString :: String -> Form
+parseString str =
+  case parse propositionalParser "" str of
+    Left e -> error $ show e
+    Right r -> r
+
+-- | parseFile function parses a file into a logical formula (untested)
+parseFile :: String -> IO Form
+parseFile file =
+  do
+    program <- readFile file
+    case parse propositionalParser "" program of
+      Left e -> print e >> fail "parse error"
+      Right r -> return r
+
+--------------
+-- PL STUFF --
+--------------
+
+-- | find function searches for an atoms value in a given interpretation
+find :: String -> Interpretation -> Bool
 find i s
   | null s = False
   | i == c = b
@@ -27,8 +160,8 @@ find i s
   where
     (c, b) = head s
 
--- | atoms function outputs the variables of a given proposition
-atoms :: Form -> [Char]
+-- | atoms function outputs the atoms of a given formula
+atoms :: Form -> [String]
 atoms (Const _) = []
 atoms (Atom x) = [x]
 atoms (Not p) = atoms p
@@ -37,7 +170,7 @@ atoms (Or p q) = atoms p ++ atoms q
 atoms (Imply p q) = atoms p ++ atoms q
 atoms (Iff p q) = atoms p ++ atoms q
 
--- | isSatisfied function evaluates a proposition based on a given subsitution
+-- | isSatisfied function evaluates a formula based on a given subsitution
 isSatisfied :: Interpretation -> Form -> Bool
 isSatisfied _ (Const b) = b -- Const Bool
 isSatisfied s (Atom x) = find x s -- Var Char
@@ -57,7 +190,7 @@ int2bool i
     leftOver = i `mod` 2
     b = leftOver > 0
 
--- | interps function generates all possible interpretations for proposition
+-- | interps function generates all possible interpretations for a formula
 interps :: Form -> [Interpretation]
 interps f = [zip alphabet bools | bools <- boolPerms]
   where
@@ -65,6 +198,6 @@ interps f = [zip alphabet bools | bools <- boolPerms]
     charLength = length alphabet + 1
     boolPerms = map int2bool [0 .. charLength]
 
--- | checks if a statement is a tautology
+-- | checks if a formula is a tautology
 isTautology :: Form -> Bool
 isTautology f = and [isSatisfied sub f | sub <- interps f]
