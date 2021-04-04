d9413bed5f43e30f192f63e046dffc06f637f4f8
Aidan Bailey <BLYAID001@myuct.ac.za>
2021-04-04 14:39:24 +0200

    moved show form instance

--- src/LogicTypes.hs
+++ src/LogicTypes.hs
@@ -18,3 +18,12 @@ type Model = Interpretation
 
 -- | KnowledgeBase type
 type KnowledgeBase = [Form]
+
+-- | Show instance of form
+instance Show Form where
+  show (Atom c) = show c
+  show (Not f) = "¬" ++ show f
+  show (And f1 f2) = show f1 ++ "∧" ++ show f2
+  show (Or f1 f2) = show f1 ++ "∨" ++ show f2
+  show (Imply f1 f2) = show f1 ++ "→" ++ show f2
+  show (Iff f1 f2) = show f1 ++ "↔" ++ show f2
--- src/Parser.hs
+++ src/Parser.hs
@@ -9,19 +9,6 @@ import Text.ParserCombinators.Parsec.Expr
 import Text.ParserCombinators.Parsec.Language
 import qualified Text.ParserCombinators.Parsec.Token as Token
 
----------------
--- INSTANCES --
----------------
-
--- | Show instance of form
-instance Show Form where
-  show (Atom c) = show c
-  show (Not f) = "¬" ++ show f
-  show (And f1 f2) = show f1 ++ "∧" ++ show f2
-  show (Or f1 f2) = show f1 ++ "∨" ++ show f2
-  show (Imply f1 f2) = show f1 ++ "→" ++ show f2
-  show (Iff f1 f2) = show f1 ++ "↔" ++ show f2
-
 ------------------
 -- LEXER/PARSER --
 ------------------
